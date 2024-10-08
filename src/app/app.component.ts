08c2ee2dae58d8d9bf583348dfe1dc420d904461
Yuri <yuricarvalho48@gmail.com>
2024-10-07 23:21:53 -0300

    Corrigindo o projeto para deixar no padrão stand-alone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'; // Certifique-se de importar corretamente
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms'; // Certifique-se de importar corretamente
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    ShoppingListComponent,
+  ],
 })
 export class AppComponent {
   itemForm: FormGroup;
 
   constructor() {
     this.itemForm = new FormGroup({
-      itemName: new FormControl('', [Validators.required])
+      itemName: new FormControl('', [Validators.required]),
     });
   }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
 })
 export class ShoppingListComponent {
   items = [
     { name: 'Apple', purchased: false },
-    { name: 'Milk', purchased: true }
+    { name: 'Milk', purchased: true },
   ];
 
   newItem: string = ''; // Propriedade para armazenar o novo item
 
   // Método para adicionar o item
   addItem() {
-    if (this.newItem.trim()) { // Verifica se o novo item não está vazio
+    if (this.newItem.trim()) {
+      // Verifica se o novo item não está vazio
       this.items.push({ name: this.newItem, purchased: false });
       this.newItem = ''; // Limpa o campo após adicionar
     }
